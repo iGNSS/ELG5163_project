082900de1de22f9ea712c0116cee52a864a045e5
albud187 <alfa.budiman@outlook.com>
2022-02-12 11:41:51 -0500

    imagery work

--- ros_work_5163project/displaced_stereo_vision/scripts/quadcopter_camera.py
+++ ros_work_5163project/displaced_stereo_vision/scripts/quadcopter_camera.py
@@ -7,19 +7,51 @@ import sys
 import rospy
 import cv2
 from std_msgs.msg import String
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import Image, CameraInfo
 from cv_bridge import CvBridge, CvBridgeError
+import numpy as np
+from image_geometry import PinholeCameraModel
 #change topic for overhead camera
 #need to modify launch file
 IMAGE_SUB_TOPIC = "/quadcopter/front_cam/camera/image"
-
+CAMERA_INFO_TOPIC = "quadcopter/front_cam/camera/camera_info"
+cameraModel = PinholeCameraModel()
+def find_reds(img):
+    img_hsv=cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    lower_red = np.array([0,50,50])
+    upper_red = np.array([10,255,255])
+    mask0 = cv2.inRange(img_hsv, lower_red, upper_red)
+    lower_red = np.array([170,50,50])
+    upper_red = np.array([180,255,255])
+    mask1 = cv2.inRange(img_hsv, lower_red, upper_red)
+    mask = mask0+mask1
+    red_pixels = np.argwhere(mask>0)
+    ypx = red_pixels[:,0]
+    xpx = red_pixels[:,1]
+    red_center = [np.mean(ypx), np.mean(xpx)]
+    return red_center
+    
 class image_converter:
+  camera_params = CameraInfo()
 
   def __init__(self):
     self.image_pub = rospy.Publisher("image_topic_1",Image)
 
     self.bridge = CvBridge()
     self.image_sub = rospy.Subscriber(IMAGE_SUB_TOPIC,Image,self.callback)
+    self.cam_info_sub = rospy.Subscriber(CAMERA_INFO_TOPIC,CameraInfo, self.get_camera_info)
+
+  def get_camera_info(self,data):
+      self.camera_params = data
+
+  def find_wafflebot(self):
+      cameraModel.fromCameraInfo(self.camera_params)
+      cameraModel.width = 640
+      cameraModel.height = 480
+
+      point = (0,1,0)
+      camera_point = cameraModel.project3dToPixel(point)
+      print(camera_point)
 
   def callback(self,data):
     try:
@@ -35,6 +67,8 @@ class image_converter:
     cv2.waitKey(3)
 
     try:
+      #print(self.camera_params)
+      self.find_wafflebot()
       self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
     except CvBridgeError as e:
       print(e)
