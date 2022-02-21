5df860350e02c0736f4dad4d13b952f8b2ceced6
albud187 <alfa.budiman@outlook.com>
2022-02-21 17:49:13 -0500

    wb_driver rotation

--- ros_work_5163project/displaced_stereo_vision/scripts/wafflebot_driver.py
+++ ros_work_5163project/displaced_stereo_vision/scripts/wafflebot_driver.py
@@ -3,7 +3,8 @@
 #package imports
 import numpy as np
 import rospy
-from cv_bridge import CvBridge, CvBridgeError
+from tf.transformations import euler_from_quaternion, quaternion_from_euler
+import image_geometry as ig
 
 #message imports
 from geometry_msgs.msg import Pose
@@ -13,33 +14,38 @@ from geometry_msgs.msg import Twist
 from sensor_msgs.msg import CameraInfo
 
 #subscribed topics
-TS_BURGERBOT_PX_WB = "/wafflebot/camera/rgb/burgerbot_px"
 TS_BURGERBOT_DIST = "/burgerbot_distance"
 TS_BB_DIR = "/wafflebot/camera/rgb/burgerbot_dir"
-
+TS_WB_ODO = "/wafflebot/odom"
 TS_BURGERBOT_DIST_L = "/burgerbot_distance_lidar"
 
+TS_BURGERBOT_PX_WB = "/wafflebot/camera/rgb/burgerbot_px"
+
+TS_WB_CAM_INFO = "/wafflebot/camera/rgb/camera_info"
 #published topics
 TP_WB_CMDVEL = "/wafflebot/cmd_vel"
 
 #global variables
+
 burgerbot_distance = Float32()
-burgerbot_px_WB = Pose()
 burgerbot_dir = Pose()
+burgerbot_px = Pose()
+wb_pose = Odometry()
 wb_vel = Twist()
+WB_Cam_info = CameraInfo()
+WB_CAM = ig.PinholeCameraModel()
+angle2BB = 0
+
 #constants
 DISTANCE_TOL = 0.05
 ANGLE_TOL = 0.05
 FOLLOWING_DISTANCE = 0.5
 
-#callbacks
-def handleCAM_WB(msg):
-    global camInfo_WB
-    camInfo_WB = msg
+#from camera wafflebot/camera/rgb/camera_info
+CAM_CENTER_X = 1920/2
+F_LEN = 1206.8897719532354
 
-def handleBBPX_WB(msg):
-    global burgerbot_px_WB
-    burgerbot_px_WB = msg
+#callbacks
 
 def handleDistance(msg):
     global burgerbot_distance
@@ -49,37 +55,62 @@ def handleBB_Dir(msg):
     global burgerbot_dir
     burgerbot_dir = msg
 
+def handleOdo_WB(msg):
+    global wb_pose
+    wb_pose = msg
+
+def handleBB_PX(msg):
+    global burgerbot_px
+    burgerbot_px = msg
+
+def Process_WB_CAM(msg):
+    global WB_Cam_info
+    global WB_CAM
+    WB_Cam_info = msg
+    WB_CAM.fromCameraInfo(msg)
+    raw_coord = (burgerbot_px.position.x, burgerbot_px.position.y)
+    rec_coord = WB_CAM.rectifyPoint(raw_coord)
+    vec2BB = WB_CAM.projectPixelTo3dRay(rec_coord)
+
+    global angle2BB
+    angle2BB = np.arctan2(vec2BB[0], vec2BB[2])
+
+
 #functions
 def motion_control():
     #refer navigate_robot example from ELG228 assignment 3
     global wb_vel
-    if burgerbot_distance > FOLLOWING_DISTANCE:
-        pass
+    wb_vel.angular.z = angle2BB
+
+    # if burgerbot_distance > FOLLOWING_DISTANCE:
+    #     pass
 
     #rotation control
     #speed control
     pass
 
-def find_angle():
-    #find xy orientation of camera
-    #get xy portion of unit vector to burgerbot
-    #get angle between these 2 vectors
-    pass
-
-
 #node
 def wafflebot_driver():
     #init
     rospy.init_node("wafflebot_driver", anonymous = False)
-    rate = rospy.Rate(60)
+    rate = rospy.Rate(20)
 
     #subscribers
-    rospy.Subscriber(TS_BURGERBOT_PX_WB, Pose, handleBBPX_WB)
-    rospy.Subsriber(TS_BURGERBOT_DIST, Float32, handleDistance)
+    rospy.Subscriber(TS_BURGERBOT_DIST, Float32, handleDistance)
     rospy.Subscriber(TS_BB_DIR, Pose, handleBB_Dir)
+    rospy.Subscriber(TS_BURGERBOT_PX_WB, Pose, handleBB_PX)
+    rospy.Subscriber(TS_WB_CAM_INFO, CameraInfo, Process_WB_CAM)
 
     #publishers
     velpub = rospy.Publisher(TP_WB_CMDVEL, Twist, queue_size=10)
+
+    while not rospy.is_shutdown():
+
+        motion_control()
+        velpub.publish(wb_vel)
+        print(wb_vel)
+        rate.sleep()
+    rospy.spin()
 
 
 if __name__ == '__main__':
